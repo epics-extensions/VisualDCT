epics-extensions/VisualDCT

commit 2922eb8fad14a7c78f429c1a2a788409ffb19ddb

Small fixes suggested by static code analysis
- reported by SonarQube

src/main/java/com/cosylab/vdct/GenerateFlatDatabase.java
@@ -175,8 +175,9 @@ public class GenerateFlatDatabase
 					}
 				}
 
-			// no break
-			case 1:
+				break;  // unreachable, but makes static code analysis happy
+
+                        case 1:
 				vdbFile = new File(args[i]);
 				state = 2;
 

src/main/java/com/cosylab/vdct/PluginManagerDialog.java
@@ -212,7 +212,8 @@ public Object getValueAt(int row, int column)
 				return STARTED_PLUGIN;
 			else if(status == PluginObject.PLUGIN_STOPPED)
 				return STOPPED_PLUGIN;
-		}
+			break;  // Makes static analysis happy
+                }
 		case(2):
 			return plugin.getName();
 		case(3):

src/main/java/com/cosylab/vdct/VisualDCT.java
@@ -133,6 +133,8 @@ import com.cosylab.vdct.util.DBDEntry;
 import com.cosylab.vdct.util.UniversalFileFilter;
 import com.cosylab.vdct.vdb.VDBData;
 import com.cosylab.vdct.vdb.VDBTemplate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This type was generated by a SmartGuide.
@@ -1977,7 +1979,6 @@ public void exportPostScriptFileMenuItem_ActionPerformed()
 				fileOutputStream = new FileOutputStream(outputFile);
 				fileOutputStream.write(printGrid(pi.getPageable().getPageFormat(0),
 						new StringBuffer(byteArrayOutputStream.toString()), view));
-				fileOutputStream.close();
 			}
 			catch(Exception exception)
 			{
@@ -1986,6 +1987,11 @@ public void exportPostScriptFileMenuItem_ActionPerformed()
 			}
 			finally
 			{
+				try {
+					if (fileOutputStream != null) fileOutputStream.close();
+				} catch (IOException ex) {
+					handleException(ex);
+				}
 				setCursor(java.awt.Cursor.getPredefinedCursor(java.awt.Cursor.DEFAULT_CURSOR));
 				Settings.getInstance().setShowGrid(showGrid);
 		

commit 11d4864a7bacbc21f10d3a7d76935443c654bb11

Fix GenerateFlatDatabase command line parser
- Bug was introduced with 2922eb8 in v2.8.3
- fixes #32

src/main/java/com/cosylab/vdct/GenerateFlatDatabase.java
@@ -122,24 +122,20 @@ public class GenerateFlatDatabase
 
 		for (int i = 0; i < args.length; i++) {
 			switch (state) {
+
 			case 0:
+                if (args[i].equals("--")) {
+                    state = 1;
+                    break;
+                }
 
-				if (args[i].equals("--")) {
-					state = 1;
-
-					break;
-				}
-
-				if (args[i].startsWith("-")) {
+                if (args[i].startsWith("-")) {
 					if (args[i].equals("-d") || args[i].equals("--dbd-file")) { //dbd file
 						i++;
-
 						if (i >= args.length) {
 							System.err.println("Missing .dbd file name.");
-
 							return false;
 						}
-
 						dbdEntries.add(new DBDEntry(args[i]));
 						continue;
 					} else if (args[i].equals("--enable-capfast")) {
@@ -157,27 +153,23 @@ public class GenerateFlatDatabase
 					} else if (args[i].equals("--disable-macro-warnings")) {
 						Settings.getInstance().setDisableMacroWarnings(true);
 						continue;							
+
 					} else if (args[i].equals("-h") || args[i].equals("--help")) {
 						printHelp();
-
 						return false;
-						
 					} else if (args[i].equals("-v")
 					    || args[i].equals("--version")) {
 						printVersion();
-
 						return false;
 					} else {
 						System.err.println("Invalid parameter: " + args[i]+"\n");
 						printHelp();
-						
 						return false;
 					}
 				}
+                // Intentionally no break here
 
-				break;  // unreachable, but makes static code analysis happy
-
-                        case 1:
+            case 1:
 				vdbFile = new File(args[i]);
 				state = 2;
 